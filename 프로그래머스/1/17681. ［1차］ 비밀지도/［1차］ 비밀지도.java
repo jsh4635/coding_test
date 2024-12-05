bc894dad134c69d145327c77c426591d3e7dedc9
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2024-12-05 15:13:36 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.16 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        for(int i = 0; i < n; i++){
+            StringBuffer sb = new StringBuffer();
+            for(int j = 0; j < n; j++){
+                if(arr1[i] % 2 == 1 || arr2[i] % 2 == 1) sb.append("#");
+                else sb.append(" ");
+                arr1[i] /= 2;
+                arr2[i] /= 2;
+            }
+            answer[i] = sb.reverse().toString();
+        }
+        
+        return answer;
+    }
+}
