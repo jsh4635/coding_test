2a8518e8a1a5c82e5c768df4858677ef57e00f1b
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2024-12-05 15:08:10 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        
+        int n = x;
+        int sum = 0;
+        
+        while(n > 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        
+        return x % sum == 0;
+    }
+}
