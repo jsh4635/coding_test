a45776fd124e160d8d9619ffa42da0b76ced11c4
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2025-01-08 09:45:37 +0900

    [level 2] Title: 숫자 변환하기, Time: 162.06 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,35 +1,6 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int x, int y, int n) {
-        int answer = 0;
-        
-        Queue<Value> q = new LinkedList<>();
-        boolean[] visited = new boolean[y - x + 1];
-        q.offer(new Value(x, 0));
-        visited[0] = true;
-        
-        while(!q.isEmpty()){
-            Value v = q.poll();
-            if(v.x == y) return v.count;
-            
-            if(v.x + n <= y && !visited[v.x + n - x]) {
-                q.offer(new Value(v.x + n, v.count + 1));
-                visited[v.x + n - x] = true;
-            }
-            if(v.x * 2 <= y && !visited[v.x * 2 - x]) {
-                q.offer(new Value(v.x * 2, v.count + 1));
-                visited[v.x * 2 - x] = true;
-            }
-            if(v.x * 3 <= y && !visited[v.x * 3 - x]) {
-                q.offer(new Value(v.x * 3, v.count + 1));
-                visited[v.x * 3 - x] = true;
-            }
-        }
-        
-        return -1;
-    }
-    
     class Value{
         int x;
         int count;
@@ -37,5 +8,33 @@ class Solution {
             this.x = x;
             this.count = count;
         }
+    }
+    
+    public int solution(int x, int y, int n) {
+        boolean[] visited = new boolean[y - x + 1];
+        
+        Queue<Value> q = new LinkedList<>();
+        q.offer(new Value(x, 0));
+        
+        while(!q.isEmpty()){
+            Value v = q.poll();
+            
+            if(v.x == y) return v.count;
+            
+            if(v.x + n <= y && !visited[v.x + n - x]){
+                visited[v.x + n - x] = true;
+                q.offer(new Value(v.x + n, v.count + 1));
+            }
+            if(v.x * 2 <= y && !visited[v.x * 2 - x]){
+                visited[v.x * 2 - x] = true;
+                q.offer(new Value(v.x * 2, v.count + 1));
+            }
+            if(v.x * 3 <= y && !visited[v.x * 3 - x]){
+                visited[v.x * 3 - x] = true;
+                q.offer(new Value(v.x * 3, v.count + 1));
+            }
+        }
+        
+        return -1;
     }
 }
