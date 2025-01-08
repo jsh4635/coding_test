ae024df36fd15ee0c09970a3b95efb7fbca50a6b
jsh4635 <49675179+jsh4635@users.noreply.github.com>
2025-01-08 09:48:50 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.88 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int n) {
         int[] arr = new int[n + 1];
-        for(int i = 0; i < n + 1; i++){
-            if(i < 4) arr[i] = i % 1000000007;
+        for(int i = 0; i <= n; i++){
+            if(i < 3) arr[i] = i % 1000000007;
             else arr[i] = (arr[i - 2] + arr[i - 1]) % 1000000007;
         }
         
